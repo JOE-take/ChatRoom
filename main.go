bb566a9329dd270262243d6e188e7e8d9ebe1224
joe <okoshi811@gmail.com>
2023-05-29 01:03:49 +0900

    loginページの追加、機能は未実装

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -13,6 +14,7 @@ func main() {
 
 	content = make([]string, 0, 16)
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/login", login)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -24,11 +26,29 @@ func main() {
 func index(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("view/index.html")
 
-	if r.Method == "POST" {
-		content = append(content, r.FormValue("message"))
-		//同じ場所にリダイレクトすることによって更新でフォームが送られるのを防ぐ
+	if v := r.FormValue("message"); r.Method == "POST" && v != "" {
+		content = append(content, v)
+		fmt.Printf("[%s]\n", v)
+		//同じ場所にリダイレクトすることによって更新でPOSTが送られるのを防ぐ
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 
 	t.Execute(w, content)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("view/login.html")
+
+	if r.Method == "POST" {
+		userName := r.FormValue("userName")
+		email := r.FormValue("email")
+		//パスワードの扱いは変更が必要
+		password := r.FormValue("password")
+		fmt.Println("userName: ", userName)
+		fmt.Println("email: ", email)
+		fmt.Println("password: ", password)
+		http.Redirect(w, r, r.URL.Path, http.StatusSeeOther)
+	}
+
+	t.Execute(w, "")
 }
